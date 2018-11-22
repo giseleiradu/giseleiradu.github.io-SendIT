ea0ca6d8b08b3eb928d81b36c3cb2388f7545762
Gisele Iradukunda <giseleiradukunda@rocketmail.com>
2018-11-22 22:56:37 +0200

    Adjust on colors

--- UI/js/dash.js
+++ UI/js/dash.js
@@ -94,3 +94,23 @@ function view(evt, idName){
 } 
 
 // document.getElementById('defaultOpen').style.display="block";
+
+//User Dash JS
+
+function openTab(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+
+    }
+    tablinks = document.getElementsByClassName('tablinks');
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace("active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    document.querySelector(".entrance").style.display= "none";
+    evt.currentTarget.className += " active";
+}
+
+document.getElementById('defaultOpen').click();
